9955ab4989bc77b73a2f22b9ca8871083bd1b128
Eric Rochester <err8n@virginia.edu>
2011-09-07 08:56:37 -0400

    Renamed source file for bakers12 utility.
    --HG--
    rename : src/Bakers12.lhs => src/Bakers12.hs

--- src/Bakers12.hs
+++ src/Bakers12.hs
@@ -1,9 +1,6 @@
 
-\begin{code}
 module Main (main) where
-\end{code}
 
-\begin{code}
 import Bakers12.Cli
 import Bakers12.Tokenizer
 
@@ -11,13 +8,10 @@ import qualified Data.Char as C
 import qualified Data.List as L
 import Text.Bakers12.Tokenizer
 import Text.Bakers12.Tokenizer.String ()
-\end{code}
 
-\begin{code}
 main :: IO ()
 main = do
     mode <- cmdArgs bakers12Modes
     case mode of
         Tokenize files -> tokenize files
-\end{code}
 
