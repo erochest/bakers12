111492f9046d8222064f012b9ad5f04840597beb
Eric Rochester <err8n@virginia.edu>
2011-09-16 15:01:50 -0400

    Added tests for frequencies.

--- tests/Test/Bakers12/Stats.hs
+++ tests/Test/Bakers12/Stats.hs
@@ -2,63 +2,61 @@
 module Test.Bakers12.Stats (statsTests) where
 
 import qualified Data.List as L
+import qualified Data.Map as M
 import           Test.HUnit (Assertion, assertBool)
 import           Test.Framework (Test, testGroup)
 import           Test.Framework.Providers.HUnit (testCase)
 import           Test.Framework.Providers.QuickCheck2 (testProperty)
-import           Text.Bakers12.Tokenizer
-import           Text.Bakers12.Tokenizer.String ()
 import           Text.Bakers12.Stats
 
--- property: ratio is always <= 1
-pFullRatioFractional :: String -> Bool
-pFullRatioFractional input =
-    L.and . map (1.0 >=) $ ratios
-    where
-        tokens = addTypeTokenRatio . fullTokenize "<pRatioFractional>" $ input
-        ratios = map snd tokens
+ratios :: String -> [Double]
+ratios = map snd . addTypeTokenRatio
 
-pFastRatioFractional :: String -> Bool
-pFastRatioFractional input =
-    L.and . map (1.0 >=) $ ratios
-    where
-        tokens = addTypeTokenRatio . fastTokenize $ input
-        ratios = map snd tokens
+-- property: ratio is always <= 1
+pRatioFractional :: String -> Bool
+pRatioFractional = L.and . map (1.0 >=) . ratios
 
 -- unit: a b c d e f g h i j k l m n o p q r s t u v w x y z
-assertNoDuplicatesFull :: Assertion
-assertNoDuplicatesFull =
+assertNoDuplicates :: Assertion
+assertNoDuplicates =
     assertBool "Type-to-token ratio is always one if there are no duplicate tokens."
-               (L.all (1 ==) ratios)
+               (L.all (1 ==) . ratios $ input)
     where
-        input = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
-        ratios = map snd . addTypeTokenRatio . fullTokenize "<assertNoDuplicates>" $ input
+        input = "abcdefghijklmnopqrstuvwxyz"
 
-assertNoDuplicatesFast :: Assertion
-assertNoDuplicatesFast =
-    assertBool "Type-to-token ratio is always one if there are no duplicate tokens."
-               (L.all (1 ==) ratios)
+-- test case, frequencies
+assertFrequencies1 :: Assertion
+assertFrequencies1 =
+    assertBool "All frequencies are one."
+               (L.all (1 ==) . map snd . M.toList $ freqs)
     where
-        input = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
-        ratios = map snd . addTypeTokenRatio . fastTokenize $ input
+        input = "abcdefghijklmnopqrstuvwxyz"
+        freqs = frequencies input
 
--- property, the ratios match
-pMatchRatios :: String -> Bool
-pMatchRatios input =
-    L.and $ L.zipWith (==) full fast
-    where full = map snd . addTypeTokenRatio . fullTokenize "<pMatchRatios>" $ input
-          fast = map snd . addTypeTokenRatio . fastTokenize $ input
+assertFrequencies2 :: Assertion
+assertFrequencies2 =
+    assertBool "All frequencies are one."
+               (L.all (2 ==) . map snd . M.toList $ freqs)
+    where
+        input = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+        freqs = frequencies input
+
+assertFrequencies3 :: Assertion
+assertFrequencies3 =
+    assertBool "All frequencies are one."
+               (L.all (3 ==) . map snd . M.toList $ freqs)
+    where
+        input = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+        freqs = frequencies input
 
 statsTests :: [Test]
 statsTests =
-    [ testGroup "fullTokenize" [ testProperty "ratio-fractional" pFullRatioFractional
-                               , testCase "no-duplicates" assertNoDuplicatesFull
+    [ testGroup "simple-stats" [ testProperty "ratio-fractional" pRatioFractional
+                               , testCase "no-duplicates" assertNoDuplicates
+                               , testCase "frequencies-1" assertFrequencies1
+                               , testCase "frequencies-2" assertFrequencies2
+                               , testCase "frequencies-3" assertFrequencies3
                                ]
-    , testGroup "fastTokenizer" [ testProperty "ratio-fractional" pFastRatioFractional
-                                , testCase "no-duplicates" assertNoDuplicatesFast
-                                ]
-    , testGroup "both-tokenizers" [ testProperty "ratios-match" pMatchRatios
-                                  ]
     ]
 
 
