bce68b7b545fb7d27ded72e13c98b03de03b402a
Eric Rochester <err8n@virginia.edu>
2011-09-16 15:34:47 -0400

    Added tests for Text.Bakers12.Stats.summarize.

--- tests/Test/Bakers12/Stats.hs
+++ tests/Test/Bakers12/Stats.hs
@@ -12,6 +12,9 @@ import           Text.Bakers12.Stats
 ratios :: String -> [Double]
 ratios = map snd . addTypeTokenRatio
 
+summaryRatios :: String -> [Double]
+summaryRatios = map snd . fst . summarize
+
 -- property: ratio is always <= 1
 pRatioFractional :: String -> Bool
 pRatioFractional = L.and . map (1.0 >=) . ratios
@@ -35,7 +38,7 @@ assertFrequencies1 =
 
 assertFrequencies2 :: Assertion
 assertFrequencies2 =
-    assertBool "All frequencies are one."
+    assertBool "All frequencies are two."
                (L.all (2 ==) . map snd . M.toList $ freqs)
     where
         input = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
@@ -43,11 +46,43 @@ assertFrequencies2 =
 
 assertFrequencies3 :: Assertion
 assertFrequencies3 =
-    assertBool "All frequencies are one."
+    assertBool "All frequencies are three."
                (L.all (3 ==) . map snd . M.toList $ freqs)
     where
         input = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
         freqs = frequencies input
+
+-- test cases for summaryStats
+assertSumNoDups :: Assertion
+assertSumNoDups =
+    assertBool "Type/token is always one if there are no duplicates tokens."
+               (L.all (1 ==) . summaryRatios $ input)
+    where
+        input = "abcdefghijklmnopqrstuvwxyz"
+
+assertSumFreqs1 :: Assertion
+assertSumFreqs1 =
+    assertBool "All frequencies are one."
+               (L.all (1 ==) . map snd . M.toList $ freqs)
+    where
+        input = "abcdefghijklmnopqrstuvwxyz"
+        freqs = snd $ summarize input
+
+assertSumFreqs2 :: Assertion
+assertSumFreqs2 =
+    assertBool "All frequencies are two."
+               (L.all (2 ==) . map snd . M.toList $ freqs)
+    where
+        input = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+        freqs = snd $ summarize input
+
+assertSumFreqs3 :: Assertion
+assertSumFreqs3 =
+    assertBool "All frequencies are three."
+               (L.all (3 ==) . map snd . M.toList $ freqs)
+    where
+        input = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+        freqs = snd $ summarize input
 
 statsTests :: [Test]
 statsTests =
@@ -56,6 +91,10 @@ statsTests =
                                , testCase "frequencies-1" assertFrequencies1
                                , testCase "frequencies-2" assertFrequencies2
                                , testCase "frequencies-3" assertFrequencies3
+                               , testCase "summary-stats" assertSumNoDups
+                               , testCase "summary-stats" assertSumFreqs1
+                               , testCase "summary-stats" assertSumFreqs2
+                               , testCase "summary-stats" assertSumFreqs3
                                ]
     ]
 
