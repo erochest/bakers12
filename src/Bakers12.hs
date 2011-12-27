2507a8383890ab9137eabc106fefa375bb835fee
Eric Rochester <err8n@virginia.edu>
2011-12-27 10:55:33 -0500

    Razed it to the ground. Back to 'Hello, World.'
    --HG--
    branch : monad-stack

--- src/Bakers12.hs
+++ src/Bakers12.hs
@@ -1,18 +1,8 @@
 
 module Main (main) where
 
-import Bakers12.Cli
-import Bakers12.Snap
-import Bakers12.Tokenizer
-import Bakers12.Frequencies
-
-import System.Bakers12.Utils (normalizeFilePaths)
 
 main :: IO ()
 main = do
-    mode <- cmdArgs bakers12Modes
-    case mode of
-        Tokenize inputs idattr -> normalizeFilePaths inputs >>= tokenize idattr
-        Freq inputs            -> normalizeFilePaths inputs >>= frequencies
-        Serve httpPort         -> serveSnap httpPort
+    putStrLn "bakers12"
 
--- tests/TestBakers12.hs
+++ tests/TestBakers12.hs
@@ -1,11 +1,7 @@
 
 module Main where
 
-import Test.Bakers12.ANC
-import Test.Bakers12.Tokenizer
-import Test.Bakers12.Stats
-
-import Test.Framework (Test, defaultMain, testGroup)
+import Test.Framework (Test, defaultMain)
 
 
 main :: IO ()
@@ -13,8 +9,6 @@ main = defaultMain tests
 
 tests :: [Test]
 tests =
-    [ testGroup "tokenizer" tokenizerTests
-    , testGroup "ANC-match" ancTests
-    , testGroup "statistics" statsTests
+    [ 
     ]
 
