e9f64f82c97b7528e0aef26391de877800c431fe
Eric Rochester <erochest@gmail.com>
2012-01-06 12:18:16 -0500

    Handle Penn TB curly braces.
    --HG--
    branch : monad-stack

--- lib/Text/Bakers12/Tokenizer/PennTreebank.hs
+++ lib/Text/Bakers12/Tokenizer/PennTreebank.hs
@@ -106,6 +106,8 @@ penn tlist@(t:ts)
     | T.singleton ')' == tokenText t = t { tokenText = T.pack "-RRB-" } : penn ts
     | T.singleton '[' == tokenText t = t { tokenText = T.pack "-LSB-" } : penn ts
     | T.singleton ']' == tokenText t = t { tokenText = T.pack "-RSB-" } : penn ts
+    | T.singleton '{' == tokenText t = t { tokenText = T.pack "-LCB-" } : penn ts
+    | T.singleton '}' == tokenText t = t { tokenText = T.pack "-RCB-" } : penn ts
     | otherwise = t : penn ts
     where (take2, drop2) = L.splitAt 2 tlist
           (take3, drop3) = L.splitAt 3 tlist
