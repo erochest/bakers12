8f8ce035e84ee4a9870ac480ac5371b5d383b69d
Eric Rochester <err8n@virginia.edu>
2012-03-07 23:02:02 -0500

    Backed out of verbose comments.

--- lib/Text/Bakers12/Tokenizer.hs
+++ lib/Text/Bakers12/Tokenizer.hs
@@ -1,15 +1,21 @@
 
--- This implements the tokenizer. It returns all tokens, without throwing any
--- text away. That way, the unprocessed token stream can be used to re-create
--- the input.
--- 
--- The processing uses the
--- [enumerator](http://hackage.haskell.org/package/enumerator) library to
--- produce a stream of tokens from a stream of input or to convert an input
--- stream into a stream of `Token` instances.
--- 
--- The `tokenize` and `tokenizeFile` functions hide this by accumulating the
--- output tokens into a list and returning that.
+{-|
+ - Module      : Text.Bakers12.Tokenizer
+ - License     : Apache 2.0
+ - Maintainer  : erochest@gmail.com
+ - Portability : GHC
+ - 
+ - This implements the tokenizer. It returns all tokens, without throwing any
+ - text away. That way, the unprocessed token stream can be used to re-create
+ - the input.
+ - 
+ - The processing uses the
+ - [enumerator](http://hackage.haskell.org/package/enumerator) library to
+ - produce a stream of tokens from a stream of input.
+ - 
+ - The `tokenize` and `tokenizeFile` functions hide this by accumulating the
+ - output tokens into a list and returning that.
+ -}
 
 module Text.Bakers12.Tokenizer
     ( Token(..)
@@ -31,30 +37,20 @@ import qualified Data.Enumerator.List as EL
 import qualified Data.Enumerator.Text as ET
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as LT
--- [Text.Bakers12.Tokenizer.Types](Tokenizer/Types.html)<br />
 import           Text.Bakers12.Tokenizer.Types
 
--- This reads text from an instance of Data.Text.Text and returns a list of
+-- | This reads text from an instance of Data.Text.Text and returns a list of
 -- Token instances.
---
--- `Either SomeException` is a standard way to define a computation that may
--- return something (in this case, a list of tokens) or may have an error.
 tokenize :: FilePath -> T.Text -> Either SomeException [Token]
 tokenize source input =
     E.runLists [[input]] (tokenizeStream source 0 E.=$ EL.consume)
 
--- This reads the input from a file and returns a list of Token instances.
---
--- Because this has to read from the file system, it has to execute in the
--- context of the `IO` monad. Otherwise, this is the same as `tokenize`.
+-- | This reads the input from a file and returns a list of Token instances.
 tokenizeFile :: FilePath -> IO (Either SomeException [Token])
 tokenizeFile inputFile =
     E.run (tokenizeFileStream inputFile E.$$ EL.consume)
 
--- This creates an `Enumerator` that reads from a file and produces `Token`
--- instances.
---
--- Enumerators produce data for an enumerator processing pipeline.
+-- | This creates an Enumerator that reads from a file and produces Tokens.
 --
 -- This assumes that the files are UTF8.
 tokenizeFileStream :: FilePath -> E.Enumerator Token IO b
@@ -63,99 +59,62 @@ tokenizeFileStream inputFile =
     ET.decode ET.utf8 E.$=
     tokenizeStream inputFile 0
 
--- This is an `Enumeratee` that takes `FilePath` and returns a `Enumerator` of
--- `Token` instances.
---
--- `Enumeratees` are links in the pipeline between an `Enumerator` and an
--- `Iteratee`. They take input coming in and filter or transform it and pass it
--- on. Because they're in the middle, they have to pay more attention to the
--- enumerator API. I'll take some time to explain how that works as we
--- encounter it.
+-- | This is an Enumeratee that takes a FilePath and returns a Enumerator of
+-- Tokens.
 tokenizeE :: E.Enumeratee FilePath Token IO b
--- First, one of the values that an enumeratee can accept is a continuation.
--- This means that there is another chunk of data available.
 tokenizeE cont@(E.Continue _) = do
-    -- This returns the first item waiting on the input stream. `Continue`
-    -- means that there is more data to come, but we may not have it right now
-    -- (say for networking).  So `EL.head` may return data, or it may not. In
-    -- Haskell, this means a `Maybe` data type.
     maybeFP <- EL.head
     case maybeFP of
-        -- If there is data right now, it's a file path. We just pass it to
-        -- `tokenizeFileStream`, which returns a bunch of tokens, and then
-        -- loop.
         Just filePath -> tokenizeFileStream filePath cont E.>>== tokenizeE
-        -- If there is no data at the moment, just continue waiting.
-        Nothing       -> return cont
--- Other enumeratee inputs mean other things (no more data, end of stream,
--- etc.), but we don't care about that.
+        Nothing -> return cont
 tokenizeE step = return step
 
--- *(After explaining this, there's an obvious way to cut down on the
--- boilerplate for this, at least for this application. It won't happen today,
--- however.)*
-
--- This is an Enumeratee that takes a stream of Char and transforms it into a
+-- | This is an Enumeratee that takes a stream of Char and transforms it into a
 -- stream of Token.
---
--- `FilePath` and `Integer` are the current state of the tokenizer. This should
--- probably be wrapped in a `State` monad and stacked on top of this, but for
--- the moment, this is simpler.
 tokenizeStream :: Monad m =>
                   FilePath -> Integer -> E.Enumeratee T.Text Token m b
 tokenizeStream source offset cont@(E.Continue k) = do
-    -- This looks at the first character from the input ...
     maybeC <- ET.head
     case maybeC of
         Just c  -> do
-            -- ... and dispatches to `tokenize'` based on the first character.
-            -- It passes the token into the output stream and loops to take
-            -- care of the next token.
             token <- tokenize' source offset c
             next  <- lift $ E.runIteratee $ k $ E.Chunks [token]
             tokenizeStream source (offset + fromIntegral (tokenLength token)) next
         Nothing -> return cont
 tokenizeStream _ _ step = return step
 
--- This takes a character and dispatches the handle the tokenizing the rest
+-- | This takes a character and dispatches the handle the tokenizing the rest
 -- of the token from it.
 tokenize' :: Monad m => FilePath -> Integer -> Char -> E.Iteratee T.Text m Token
 tokenize' source offset c
-    -- Tokens that contain alphanumeric or spaces/separators return a span of
-    -- tokens of the same type.
     | C.isAlpha c       = tokenFromTaken source offset AlphaToken c C.isAlpha
     | C.isNumber c      = tokenFromTaken source offset NumberToken c C.isNumber
     | isSeparator c     = tokenFromTaken source offset SeparatorToken c isSeparator
-
-    -- Punctuation, symbols, marks, and what-have-yous are single-character
-    -- tokens.
     | C.isPunctuation c = return . makeToken source offset PunctuationToken $ T.singleton c
     | C.isSymbol c      = return . makeToken source offset SymbolToken $ T.singleton c
     | C.isMark c        = return . makeToken source offset MarkToken $ T.singleton c
     | otherwise         = return . makeToken source offset UnknownToken $ T.singleton c
 
--- This is an augmented separator predicate that also tests for spaces.
+-- | This is an augmented separator predicate that also tests for spaces.
 isSeparator :: Char -> Bool
 isSeparator c = C.isSpace c || C.isSeparator c
 
--- This runs `takeWhile` with the predicate, conses the initial element to the
--- front, and creates a Token of the given type. Hence `Token` from `takeWhile`
--- -en. Or something.
+-- | This runs takeWhile with the predicate, conses the initial element to the
+-- front, and creates a Token of the given type.
 tokenFromTaken :: Monad m   
                => FilePath                      -- ^ tokenSource
                -> Integer                       -- ^ tokenOffset
                -> TokenType                     -- ^ tokenType
                -> Char                          -- ^ Initial character.
-               -> (Char -> Bool)                -- ^ Predicate for taking the rest of the token.
+               -> (Char -> Bool)                -- ^ Predicate for taking the
+                                                -- rest of the token.
                -> E.Iteratee T.Text m Token
 tokenFromTaken source offset tType initial predicate =
-    -- The composed function here attaches the initial character, actualizes
-    -- the token string from lazy to strict, and creates the `Token` from that.
     liftM (makeToken source offset tType . LT.toStrict . LT.cons initial)
           (ET.takeWhile predicate)
 
--- This takes the minimum data necessary to create a token, normalizes the
--- input text, and creates the `Token`.
+-- | In the context of an Enumerator, this takes a [Char] list and returns a
+-- Token.
 makeToken :: FilePath -> Integer -> TokenType -> T.Text -> Token
 makeToken source offset tType raw =
     Token normalized raw rawLength tType source offset
@@ -163,7 +122,7 @@ makeToken source offset tType raw =
         normalized = normalizeToken raw
         rawLength  = T.length raw
 
--- This takes a raw token `Text` and returns a normalized version. Currently,
+-- | This takes a raw token Text and returns a normalized version. Currently,
 -- this just lower-cases everything.
 normalizeToken :: T.Text -> T.Text
 normalizeToken = T.map C.toLower
--- lib/Text/Bakers12/Tokenizer/Types.hs
+++ lib/Text/Bakers12/Tokenizer/Types.hs
@@ -1,6 +1,12 @@
 
+-- |
+-- Module      : Text.Bakers12.Tokenizer.Types
+-- License     : Apache 2.0
+-- Maintainer  : erochest@gmail.com
+-- Portability : GHC
+--
 -- This defines the types for the tokenizers. Especially, this defines the
--- `Token` and `TokenType` data types.
+-- Token and TokenType data types.
 
 module Text.Bakers12.Tokenizer.Types
     ( Token(..)
@@ -16,39 +22,45 @@ import qualified Data.List as L
 import qualified Data.Text as T
 import           Prelude hiding (concat, splitAt)
 
--- This contains data from one token instance.
+-- * Token Type
+
+-- | This contains data from one token instance.
 data Token = Token
     { tokenText   :: T.Text         -- ^ The normalized token text.
     , tokenRaw    :: T.Text         -- ^ The raw token text.
     , tokenLength :: Int            -- ^ The length of the raw token.
-    , tokenType   :: TokenType      -- ^ The type of data contained in the token.
-    , tokenSource :: FilePath       -- ^ The source (sometimes a file) that the token was found in.
-    , tokenOffset :: Integer        -- ^ The character offset of the start of the token in the source (zero-indexed).
+    , tokenType   :: TokenType      -- ^ The type of data contained in the
+                                    -- token.
+    , tokenSource :: FilePath       -- ^ The source (sometimes a file) that
+                                    -- the token was found in.
+    , tokenOffset :: Integer        -- ^ The character offset of the start of
+                                    -- the token in the source (zero-indexed).
     }
     deriving (Eq, Show)
 
--- This has the types of information that a token can contain.
+-- | This has the types of information that a token can contain.
 data TokenType =
       AlphaToken                    -- ^ Unicode alphabetic characters.
     | NumberToken                   -- ^ Unicode numeric characters.
-    | SeparatorToken                -- ^ Unicode space or Unicode separator character.
+    | SeparatorToken                -- ^ Unicode space or Unicode separator
+                                    -- character.
     | PunctuationToken              -- ^ One Unicode punctuation character.
     | SymbolToken                   -- ^ One Unicode symbol character.
     | MarkToken                     -- ^ One Unicode mark character.
     | UnknownToken                  -- ^ None of the categories above.
     deriving (Eq, Show)
 
--- This is an empty token.
+-- | This is an empty token.
 empty :: Token
 empty = Token T.empty T.empty 0 UnknownToken "" 0
 
--- This tests whether a token is "readable." I.e., whether its type is
+-- | This tests whether a token is "readable." I.e., whether its type is
 -- AlphaToken or NumberToken.
 isReadable :: Token -> Bool
 isReadable token = tType == AlphaToken || tType == NumberToken
     where tType = tokenType token
 
--- This takes two tokens (presumably right next to each other in the input
+-- | This takes two tokens (presumably right next to each other in the input
 -- stream, although this doesn't verify that) and concatenates them by
 -- concatenating their text and raw data and their lengths.
 append :: Token -> Token -> Token
@@ -58,11 +70,11 @@ append a b =
       , tokenLength = tokenLength a + tokenLength b
       }
 
--- This takes a list of tokens and appends them. Like `append` above, it
+-- | This takes a list of tokens and appends them. Like append above, it
 -- assumes that the tokens occur right next to each other.
 --
--- The implementation of this could use fold, but I thought that using
--- `T.concat` and not constructing any intermediate Tokens would be faster.
+-- The implementation of this could use fold, but I thought that using T.concat
+-- and not constructing any intermediate Tokens would be faster.
 concat :: [Token] -> Token
 concat []       = empty
 concat [t]      = t
@@ -76,12 +88,12 @@ concat ts@(a:_) =
         -- Another small optimization, so we don't have to walk the input list
         -- more than once. If these operations can be fused, that would be
         -- best, but even just walking the list twice is better than walking it
-        -- three times. (Best would be to write `unzipWith3` or something,
-        -- which would insure that the list was only walked twice. Someday.)
+        -- three times. (Best would be to write unzipWith3 or something, which
+        -- would insure that the list was only walked twice. Someday.)
         (texts, raws, lengths) = L.unzip3 . map getFields $ ts
         getFields t = (tokenText t, tokenRaw t, tokenLength t)
 
--- This splits a token on position N in the tokenText. This copies the raw
+-- | This splits a token on position N in the tokenText. This copies the raw
 -- token, unchanged, into each of the resulting tokens. However, the offset of
 -- the second token is moved up and the length of the tokens reflects the
 -- length of the split token texts.
